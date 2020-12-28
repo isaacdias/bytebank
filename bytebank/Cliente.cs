7f4fc64da93a7784063bbc69679969a16313843d
isaacdias <isaacardias@gmail.com>
2020-12-28 13:32:40 -0300

    add classe Cliente

---
+++ bytebank/Cliente.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bytebank
+{
+    class Cliente
+    {
+        private string _cpf;
+
+        public string Nome { get; set; };
+        public string CPF
+        {
+            get
+            {
+                return _cpf;
+            }
+            set
+            {
+                _cpf = value;
+            }
+        }
+        public string Profissao { get; set; }
+    }
+}
