5642cb2c1d67f2bc8a5b4b80d79bad168211c56c
isaacdias <isaacardias@gmail.com>
2020-12-28 17:01:22 -0300

    criada o projeto e classe para funcionarios

---
+++ bytebankFuncionarios/Funcionarios/Funcionario.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bytebankFuncionarios.Funcionarios
+{
+    public class Funcionario
+    {
+        public string Nome { get; set; }
+        public string CPF { get; set; }
+        public double Salario { get; set; }
+
+
+
+        public double GetBonificacao()
+        {
+            return Salario * 0.10;
+        }
+    }
+}
---
+++ bytebankFuncionarios/Program.cs
@@ -0,0 +1,25 @@
+﻿using bytebankFuncionarios.Funcionarios;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bytebankFuncionarios
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Funcionario carlos = new Funcionario();
+
+            carlos.Nome = "Carlos";
+            carlos.CPF = "123.456.789.20";
+            carlos.Salario = 2000;
+
+            Console.WriteLine(carlos.Nome);
+            Console.WriteLine(carlos.GetBonificacao());
+            Console.ReadLine();
+        }
+    }
+}
