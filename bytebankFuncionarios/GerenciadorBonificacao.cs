3ca62070d1e424dd3e807ab27ebe2300c08e8aea
isaacdias <isaacardias@gmail.com>
2020-12-28 17:32:23 -0300

    criada a classe gerenciadorBonificacao

---
+++ bytebankFuncionarios/GerenciadorBonificacao.cs
@@ -0,0 +1,29 @@
+﻿using bytebankFuncionarios.Funcionarios;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bytebankFuncionarios
+{
+    public class GerenciadorBonificacao
+    {
+        private double _totalBonitifacao;
+
+        public void Registrar(Funcionario funcionario)
+        {
+            _totalBonitifacao += funcionario.GetBonificacao();
+        }
+
+        public void Registrar(Diretor diretor)
+        {
+            _totalBonitifacao += diretor.GetBonificacao();
+        }
+
+        public double getTotalBonificacao()
+        {
+            return _totalBonitifacao;
+        }
+    }
+}
