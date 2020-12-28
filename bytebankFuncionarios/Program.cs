7a20e45f322b00d01ec9dc8771ceeae7c39bc391
isaacdias <isaacardias@gmail.com>
2020-12-28 17:33:36 -0300

    instaciando funcionario e diretor e contabilizando as bonificaçõs

--- bytebankFuncionarios/Program.cs
+++ bytebankFuncionarios/Program.cs
@@ -12,13 +12,29 @@ namespace bytebankFuncionarios
         static void Main(string[] args)
         {
             Funcionario carlos = new Funcionario();
+            GerenciadorBonificacao gerenciador = new GerenciadorBonificacao();
 
             carlos.Nome = "Carlos";
             carlos.CPF = "123.456.789.20";
             carlos.Salario = 2000;
 
+            gerenciador.Registrar(carlos);
+
+            Diretor roberta = new Diretor();
+
+            roberta.Nome = "Roberta";
+            roberta.CPF = "789.456.123.79";
+            roberta.Salario = 5000;
+            gerenciador.Registrar(roberta);
+
             Console.WriteLine(carlos.Nome);
             Console.WriteLine(carlos.GetBonificacao());
+            
+            Console.WriteLine(roberta.Nome);
+            Console.WriteLine(roberta.GetBonificacao());
+
+            Console.WriteLine("Total de bonificações: " +  gerenciador.getTotalBonificacao());
+            
             Console.ReadLine();
         }
     }
