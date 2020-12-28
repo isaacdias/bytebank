223fddf6fad48c0f87b1e29cd2e3361063400e29
isaacdias <isaacardias@gmail.com>
2020-12-28 17:45:11 -0300

    refatoração do código utilizando herança

--- bytebankFuncionarios/Funcionarios/Diretor.cs
+++ bytebankFuncionarios/Funcionarios/Diretor.cs
@@ -6,13 +6,8 @@ using System.Threading.Tasks;
 
 namespace bytebankFuncionarios.Funcionarios
 {
-    public class Diretor
+    public class Diretor : Funcionario
     {
-        public string Nome { get; set; }
-        public string CPF { get; set; }
-        public double Salario { get; set; }
-
-
         public double GetBonificacao()
         {
             return Salario;
