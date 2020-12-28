634bff139a2bdff9fab115806fba0679ab8ff0b1
isaacdias <isaacardias@gmail.com>
2020-12-28 17:51:50 -0300

    permitindo que o metodo GetBonificacao() seja sobrescrito

--- bytebankFuncionarios/Funcionarios/Funcionario.cs
+++ bytebankFuncionarios/Funcionarios/Funcionario.cs
@@ -13,7 +13,7 @@ namespace bytebankFuncionarios.Funcionarios
         public double Salario { get; set; }
 
              
-        public double GetBonificacao()
+        public virtual double GetBonificacao()
         {
             return Salario * 0.10;
         }
