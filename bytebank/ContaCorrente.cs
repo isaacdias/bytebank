bcc68d3368a755c831bad409edc5098ef0c0312b
isaacdias <isaacardias@gmail.com>
2020-12-28 13:08:32 -0300

    add projeto

---
+++ bytebank/ContaCorrente.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bytebank
+{
+    class ContaCorrente
+    {
+        public string titular;
+        public int agencia;
+        public int conta;
+        public double saLdo;
+    }
+}
---
+++ bytebank/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bytebank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente conta = new ContaCorrente();
+
+            conta.titular = "Isaac";
+            conta.agencia = 123;
+            conta.conta = 456789;
+            conta.saLdo = 100;
+
+
+            Console.WriteLine(conta.titular);
+            Console.WriteLine(conta.agencia);
+            Console.WriteLine(conta.conta);
+            Console.WriteLine(conta.saLdo);
+            Console.ReadLine();
+        }
+    }
+}
