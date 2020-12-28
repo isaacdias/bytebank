dadd75c99f586afe3070500395ef3485eca15ebc
isaacdias <isaacardias@gmail.com>
2020-12-28 17:34:13 -0300

    alterando o código após a criação das novas classes

--- bytebankFuncionarios/Funcionarios/Funcionario.cs
+++ bytebankFuncionarios/Funcionarios/Funcionario.cs
@@ -7,13 +7,12 @@ using System.Threading.Tasks;
 namespace bytebankFuncionarios.Funcionarios
 {
     public class Funcionario
-    {
+    {       
         public string Nome { get; set; }
         public string CPF { get; set; }
         public double Salario { get; set; }
 
-
-
+             
         public double GetBonificacao()
         {
             return Salario * 0.10;
