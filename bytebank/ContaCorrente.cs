81b3bdf927913a947842df6468e13b8eecbd034b
isaacdias <isaacardias@gmail.com>
2020-12-28 14:19:13 -0300

    Alteração na implementação da classe ContaCorrente

--- bytebank/ContaCorrente.cs
+++ bytebank/ContaCorrente.cs
@@ -8,9 +8,78 @@ namespace bytebank
 {
     class ContaCorrente
     {
-        public string titular;
-        public int agencia;
-        public int conta;
-        public double saLdo;
+        public Cliente titular { get; set; }
+        public static int TotalDeContasCriadas { get; private set; }
+        private int _agencia;
+        public int Agencia
+        {
+            get
+            {
+                return _agencia;
+            }
+            set
+            {
+                if(value <= 0)
+                {
+                    return;
+                }
+                _agencia = value;
+            }
+        }
+
+        public int Numero { get; set; }
+
+        private double _saldo = 100;
+        public double Saldo
+        {
+            get
+            {
+                return _saldo;
+            }
+            set
+            {
+                if(value < 0)
+                {
+                    return;
+                }
+                _saldo = value;
+            }
+        }
+
+        public ContaCorrente(int agencia, int numero)
+        {
+            Agencia = agencia;
+            Numero = numero;
+
+            TotalDeContasCriadas++;
+        }
+
+        public bool Sacar(double valor)
+        {
+            if(_saldo < valor)
+            {
+                return false;
+            }
+            _saldo-= valor;
+            return true;
+        }
+
+        public void Depositar(double valor)
+        {
+            _saldo += valor;
+        }
+
+        public bool Transferir(double valor, ContaCorrente contaDestino)
+        {
+            if (_saldo < valor)
+            {
+                return false;
+            }
+
+            _saldo -= valor;
+            contaDestino.Depositar(valor);
+            return true;
+        }
+
     }
 }
