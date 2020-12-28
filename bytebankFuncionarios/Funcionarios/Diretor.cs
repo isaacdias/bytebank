a5d4dd76073b566b957e6a3c421622abf7f69125
isaacdias <isaacardias@gmail.com>
2020-12-28 17:31:36 -0300

    criada a classe diretor

---
+++ bytebankFuncionarios/Funcionarios/Diretor.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bytebankFuncionarios.Funcionarios
+{
+    public class Diretor
+    {
+        public string Nome { get; set; }
+        public string CPF { get; set; }
+        public double Salario { get; set; }
+
+
+        public double GetBonificacao()
+        {
+            return Salario;
+        }
+    }
+}
