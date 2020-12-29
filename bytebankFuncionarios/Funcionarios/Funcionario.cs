0af79b50ab38aa90ff74df87d454d4f2849b94ab
isaacdias <isaacardias@gmail.com>
2020-12-29 11:03:36 -0300

    tornado os metodos Getbonificacao e AumentarSalario em metodos abstratos

--- bytebankFuncionarios/Funcionarios/Funcionario.cs
+++ bytebankFuncionarios/Funcionarios/Funcionario.cs
@@ -20,14 +20,9 @@ namespace bytebankFuncionarios.Funcionarios
             CPF = cpf;
             TotalDeFuncionarios++;
         }
-         
-        public virtual void AumentarSalario()
-        {
-            Salario *= 1.1;
-        }
-        public virtual double GetBonificacao()
-        {
-            return Salario * 0.10;
-        }
+
+        public abstract void AumentarSalario();
+
+        public abstract double GetBonificacao();
     }
 }
