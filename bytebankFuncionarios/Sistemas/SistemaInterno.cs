83dd121572876255be287cdff4274f28fba50560
isaacdias <isaacardias@gmail.com>
2020-12-29 11:39:58 -0300

    criada a classe de SistemaInterno

---
+++ bytebankFuncionarios/Sistemas/SistemaInterno.cs
@@ -0,0 +1,28 @@
+﻿using bytebankFuncionarios.Funcionarios;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bytebankFuncionarios.Sistemas
+{
+    public class SistemaInterno
+    {
+        public bool Logar(Funcionario funcionario, string senha)
+        {
+            bool usuarioAutenticado = funcionario.Autenticar(senha);
+
+            if (usuarioAutenticado)
+            {
+                Console.WriteLine("Bem-vindo ao sistema!");
+                return true;
+            }
+            else
+            {
+                Console.WriteLine("Senha incorreta!");
+                return false;
+            }
+        }
+    }
+}
