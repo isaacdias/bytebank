a7384dcd2a843d2ecbea17b1aa5cfb95496e6fa5
isaacdias <isaacardias@gmail.com>
2020-12-29 11:41:04 -0300

    autennticando usuarios

--- bytebankFuncionarios/Program.cs
+++ bytebankFuncionarios/Program.cs
@@ -1,4 +1,5 @@
 ﻿using bytebankFuncionarios.Funcionarios;
+using bytebankFuncionarios.Sistemas;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,27 +12,44 @@ namespace bytebankFuncionarios
     {
         static void Main(string[] args)
         {
-            CalcularBonificacao();
+            //CalcularBonificacao();
+            UsarSistema();
             Console.ReadLine();
+        }
+
+        public static void UsarSistema()
+        {
+            SistemaInterno sistemaInterno = new SistemaInterno();
+
+            Diretor roberta = new Diretor("789.456.123.11");
+            roberta.Nome = "Roberta";
+            roberta.Senha = "123";
+
+            Funcionario camila = new GerenteDeConta("123.789.456.33");
+            camila.Nome = "Camila";
+            camila.Senha = "abc";
+
+            sistemaInterno.Logar(roberta, "123");
+            sistemaInterno.Logar(camila, "abc");
         }
 
         public static void CalcularBonificacao()
         {
             GerenciadorBonificacao gerenciadorBonificacao = new GerenciadorBonificacao();
 
-            Desenvolvedor isaac = new Desenvolvedor("654.321.987.44");
+            Funcionario isaac = new Desenvolvedor("654.321.987.44");
             isaac.Nome = "Isaac";
 
-            Designer pedro = new Designer("123.456.789-00");
+            Funcionario pedro = new Designer("123.456.789-00");
             pedro.Nome = "Pedro";
 
-            Diretor roberta = new Diretor("789.456.123.11");
+            Funcionario roberta = new Diretor("789.456.123.11");
             roberta.Nome = "Roberta";
 
-            Auxiliar igor = new Auxiliar("456.123.789.22");
+            Funcionario igor = new Auxiliar("456.123.789.22");
             igor.Nome = "Igor";
 
-            GerenteDeConta camila = new GerenteDeConta("123.789.456.33");
+            Funcionario camila = new GerenteDeConta("123.789.456.33");
             camila.Nome = "Camila";
 
             gerenciadorBonificacao.Registrar(isaac);
