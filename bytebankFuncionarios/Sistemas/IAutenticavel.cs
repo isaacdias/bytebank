9199217146359884e79c81795ec72fcdfb89f55a
isaacdias <isaacardias@gmail.com>
2020-12-29 13:33:51 -0300

    criada a interface IAutenticavel

---
+++ bytebankFuncionarios/Sistemas/IAutenticavel.cs
@@ -0,0 +1,14 @@
+﻿using bytebankFuncionarios.Funcionarios;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bytebankFuncionarios.Sistemas
+{
+    public interface IAutenticavel
+    {
+        bool Autenticar(string senha);
+    }
+}
