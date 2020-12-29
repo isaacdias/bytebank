be304ff41a9cb943ce7d39b77057accd3a80f107
isaacdias <isaacardias@gmail.com>
2020-12-29 11:05:05 -0300

    add a classe Desenvolvedor

---
+++ bytebankFuncionarios/Funcionarios/Desenvolvedor.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bytebankFuncionarios.Funcionarios
+{
+    public class Desenvolvedor : Funcionario
+    {
+        public Desenvolvedor(string cpf) : base(2000, cpf)
+        {
+
+        }
+
+        public override void AumentarSalario()
+        {
+            Salario *= 0.15;
+        }
+
+        public override double GetBonificacao()
+        {
+            return Salario * 0.1;
+        }
+    }
+}
