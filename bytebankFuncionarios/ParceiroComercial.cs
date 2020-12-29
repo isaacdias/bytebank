36285ecc7a997d366bf2173e4297ed2ba4b16e02
isaacdias <isaacardias@gmail.com>
2020-12-29 13:36:16 -0300

    add classe ParceiroComercial que implementa IAutenticavel

---
+++ bytebankFuncionarios/ParceiroComercial.cs
@@ -0,0 +1,18 @@
+﻿using bytebankFuncionarios.Sistemas;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bytebankFuncionarios
+{
+    public class ParceiroComercial : IAutenticavel
+    {
+        public string Senha { get; set; }
+        public bool Autenticar(string senha)
+        {
+            return Senha == senha;
+        }
+    }
+}
