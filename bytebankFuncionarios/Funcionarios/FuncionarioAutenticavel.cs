fbc590dccb1329320862f8d3499cf9d43b0d4d66
isaacdias <isaacardias@gmail.com>
2020-12-29 13:35:35 -0300

    add a classe FuncionarioAutenticavel que herda funcionario e implementa IAutenticavel

---
+++ bytebankFuncionarios/Funcionarios/FuncionarioAutenticavel.cs
@@ -0,0 +1,25 @@
+﻿using bytebankFuncionarios.Sistemas;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bytebankFuncionarios.Funcionarios
+{
+    public abstract class FuncionarioAutenticavel : Funcionario, IAutenticavel
+    {
+        public string Senha { get; set; }
+
+        public FuncionarioAutenticavel(double salario, string cpf)
+            : base(salario, cpf)
+        {
+
+        }
+
+        public bool Autenticar(string senha)
+        {
+            return Senha == senha;
+        }
+    }
+}
